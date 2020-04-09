90e3fbe8049af2a6ae5cdf9c6a3b423c9f92b5ea
Dorian Young <DorianYoung@Ymail.com>
2020-04-08 18:00:24 -0700

    Chnaged eaten to devoured

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -18,8 +18,8 @@ router.get("/", function (req, res) {
 
 router.post("/api/burgers", function (req, res) {
   burger.insertOne(
-    ["name", "eaten"],
-    [req.body.name, req.body.eaten],
+    ["name", "devoured"],
+    [req.body.name, req.body.devoured],
     function (result) {
       // Send back the ID of the new quote
       res.json({ id: result.insertId });
